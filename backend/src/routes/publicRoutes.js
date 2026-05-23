6d4d198e3298c47e072389998ea5614dc55370c1
josepe-bit <josepabon8@gmail.com>
2026-05-22 20:01:33 -0500

    clean up db-debug endpoint for production

--- backend/src/routes/publicRoutes.js
+++ backend/src/routes/publicRoutes.js
@@ -6,6 +6,5 @@ router.get('/mensajes/activos', publicController.getActiveMessages);
 router.get('/tarifas', publicController.getTarifas);
 router.get('/torneos', publicController.getPublicTorneos);
 router.get('/parametros', publicController.getParametros);
-router.get('/db-debug', publicController.getDbDebug);
 
 module.exports = router;
